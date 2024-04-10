e1d348e7f9bb0645201aba95ee0418d7edd8eeb8
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-04-10 17:24:47 +0200

    Stampato in pagiina la lista dei Treni

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,27 @@
 
 @section('content')
 
+<div class="container py-5 ">
+    <h1 class="title py-5">Lista dei Treni : </h1>
+     <div class="trains">
+
+         @foreach ($trains as $train)
+             <ul>
+                 <li>
+                 <p>Compagnia : {{$train->company}}</p> 
+                 <p>Partenza da : {{$train->departured}}</p> 
+                 <p>Arrivo a : {{$train->arrival_station}}</p>
+                 <p>Orario di partenza : {{$train->started_time}}</p>
+                 <p>Orario di arrivo : {{$train->arrival_time}}</p>
+                 <p>Codice del treno : {{$train->train_code}}</p> 
+                 <p>Numero Carrozza : {{$train->carriage_number}}</p>
+                 <p>In orario :  @if ($train->on_time==1) Si @else in Ritardo @endif </p>
+                 <p>Cancellato :  @if ($train->deleted==1) Cancellato @else In Arrvio @endif</p>
+                </li>
+             </ul>
+         @endforeach
+
+     </div>
+</div>
+
 @endsection
