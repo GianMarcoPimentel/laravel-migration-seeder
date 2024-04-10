80c1d78c3c82da4e81d91ceb00a4ef9bbc82fbae
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-04-10 15:22:38 +0200

    composizione della tabella

--- database/migrations/2024_04_10_131036_create_trains_table.php
+++ database/migrations/2024_04_10_131036_create_trains_table.php
@@ -13,10 +13,31 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company');
+            $table->string('departured');
+            $table->string('arrival_station');
+            $table->time('started_time');
+            $table->time('arrival_time');
+            $table->string('train_code')->unique();
+            $table->integer('carriage_number');
+            $table->boolean('on_time')->default(true);
+            $table->boolean('deleted')->default(false);
             $table->timestamps();
         });
     }
-
+/* 
+            $table->id();
+            $table->string('azienda');
+            $table->string('stazione_partenza');
+            $table->string('stazione_arrivo');
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->string('codice_treno')->unique();
+            $table->integer('numero_carrozze');
+            $table->boolean('in_orario')->default(true);
+            $table->boolean('cancellato')->default(false);
+            $table->timestamps();
+*/
     /**
      * Reverse the migrations.
      */
