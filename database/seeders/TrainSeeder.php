999d73d5493c2f3ff01318a46638ebe25b650762
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-04-10 15:57:49 +0200

    Seeder Corretto

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -2,16 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class TrainsSeeder extends Seeder
+class TrainSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        $newTrain = new Train();
     }
 }
